ce78d7b77d5aff8bb7e687b19b457be73c499e2d
mayank0410 <65878632+mayank0410@users.noreply.github.com>
2021-02-27 16:07:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 document.querySelector('.fo').addEventListener('click', function(){
-    if (!document.querySelector('#name').value=='' || !document.querySelector('#contact').value=='' || !document.querySelector('#email').value=='' || !document.querySelector('#address').value=='') {
+    if (!document.querySelector('#name').value=='' && !document.querySelector('#contact').value=='' && !document.querySelector('#email').value=='' && !document.querySelector('#address').value=='') {
         alert('Details Submitted will contact shortly.')
     }
 })
